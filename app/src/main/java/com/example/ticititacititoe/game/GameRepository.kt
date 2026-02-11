e857af5e1bfa02cfd875361ed829c5aa1c0a82ab
Amandamandaanda <amandatanaskovic@gmail.com>
2026-02-11 22:31:52 +0100

    added firestore collection with game fields

--- app/src/main/java/com/example/ticititacititoe/game/GameRepository.kt
+++ app/src/main/java/com/example/ticititacititoe/game/GameRepository.kt
@@ -1,6 +1,48 @@
 package com.example.ticititacititoe.game
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 class GameRepository {
-    // Speaking to Firebase to fetch game info
+
+    private val firestore = FirebaseFirestore.getInstance()
+
+    fun createGame(
+        playerX : String,
+        playerO: String,
+        startingPlayer: String,
+        currentPlayer: String,
+        gameResult: String,
+        onResult: (Result<String>) -> Unit
+    ){
+        val gameId = firestore.collection("game").document().id
+
+        val game = hashMapOf(
+            "gameId" to gameId,
+            "playerX" to playerX,
+            "playerO" to playerO,
+            "startingPlayer" to startingPlayer,
+            "currentPlayer" to currentPlayer,
+            "gameResult" to gameResult,
+            "timestamp" to System.currentTimeMillis(),
+            "moves" to emptyList<Map<String, Any>>()
+        )
+
+        firestore.collection("game")
+            .document(gameId)
+            .set(game)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onResult(Result.success(gameId))
+                } else {
+                    onResult(
+                        Result.failure(
+                            task.exception
+                                ?: Exception("Game failed to start")
+                        )
+                    )
+                }
+            }
+    }
+
 
 }
