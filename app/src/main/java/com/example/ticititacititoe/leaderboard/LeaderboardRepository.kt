d95379d83f4b3bc808c01e8f6f927ad2b252b214
Nedakhalaj <neda.khalaj7168@gmail.com>
2026-02-17 14:56:52 +0100

    LeaderBoards

--- app/src/main/java/com/example/ticititacititoe/leaderboard/LeaderboardRepository.kt
+++ app/src/main/java/com/example/ticititacititoe/leaderboard/LeaderboardRepository.kt
@@ -1,4 +1,42 @@
 package com.example.ticititacititoe.leaderboard
 
+import com.example.ticititacititoe.profile.User
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.tasks.await
+
 class LeaderboardRepository {
+    private val db = Firebase.firestore
+
+    suspend fun getAllUsers(): List<User>{
+        val snapshot = db.collection("users")
+            .get()
+            .await()
+        return snapshot.documents.mapNotNull { doc ->
+            doc.toObject(User::class.java)?.copy(id = doc.id)
+        }
+
+    }
+
+
+     fun getRatings(userId: String, callback: (Double?) -> Unit){
+        val docRef = db.collection("users")
+                          .document(userId)
+                           docRef.get()
+                               .addOnSuccessListener{document ->
+                                   if(document != null && document.exists()){
+                                     val rating = document.getDouble("raiting")
+                                       callback(rating)
+                                   }else{
+                                       callback(null)
+                                   }
+                               }
+                               .addOnFailureListener {
+                                   callback(null)
+                               }
+
+
+
+
+    }
 }
--- app/src/main/java/com/example/ticititacititoe/leaderboard/LeaderboardViewModel.kt
+++ app/src/main/java/com/example/ticititacititoe/leaderboard/LeaderboardViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.ticititacititoe.leaderboard
 
-class LeaderboardViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ticititacititoe.profile.User
+import kotlinx.coroutines.launch
+
+class LeaderboardViewModel(): ViewModel() {
+    private var repository = LeaderboardRepository()
+    private val _users = MutableLiveData<List<User>>()
+    val users: LiveData<List<User>> = _users
+
+    fun getAllUsers(){
+        viewModelScope.launch {
+            try {
+                val users = repository.getAllUsers()
+                _users.value = users
+            }catch (exception: Exception){
+                _users.value = emptyList()
+            }
+        }
+    }
+
+    fun getRatings(userId: String, callback: (Double?) -> Unit){
+                 repository.getRatings(userId, callback)
+
+    }
+
+
+
+
 }
