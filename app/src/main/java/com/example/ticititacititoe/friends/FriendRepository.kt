fb54bfe4ca770fda3b8156e96e5fe7c16f9186f4
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-02-25 10:45:57 +0100

    Added repository for adding and deleting friends, speaking to firebase, and viewmodel connected to repo

--- app/src/main/java/com/example/ticititacititoe/friends/FriendRepository.kt
+++ app/src/main/java/com/example/ticititacititoe/friends/FriendRepository.kt
@@ -1,4 +1,28 @@
 package com.example.ticititacititoe.friends
 
+import com.example.ticititacititoe.profile.User
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.tasks.await
+
 class FriendRepository {
+    private val db = Firebase.firestore
+
+    suspend fun addFriend(currentUserId: String, friendId: String) {
+        db.collection("users")
+            .document(currentUserId)
+            .collection("friends")
+            .document(friendId)
+            .set(mapOf("addedAt" to System.currentTimeMillis()))
+            .await()
+    }
+
+    suspend fun deleteFriend(currentUserId: String, friendId: String) {
+        db.collection("users")
+            .document(currentUserId)
+            .collection("friends")
+            .document(friendId)
+            .delete()
+            .await()
+    }
 }
--- app/src/main/java/com/example/ticititacititoe/friends/FriendViewModel.kt
+++ app/src/main/java/com/example/ticititacititoe/friends/FriendViewModel.kt
@@ -1,4 +1,40 @@
 package com.example.ticititacititoe.friends
 
-class FriendViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ticititacititoe.game.invitations.GameInvitationRepository
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+
+class FriendViewModel: ViewModel() {
+    private val repository = FriendRepository()
+    private val _errorEvents = MutableSharedFlow<String>()
+    val errorEvents = _errorEvents.asSharedFlow()
+
+    fun addFriend(currentUserId: String, friendId: String) {
+        viewModelScope.launch {
+            try {
+                repository.addFriend(currentUserId, friendId)
+            } catch (e: Exception){
+                _errorEvents.emit("Failed to add friend: $friendId, try again!")
+
+            }
+        }
+    }
+
+
+    fun deleteFriend(currentUserId: String, friendId: String) {
+        viewModelScope.launch {
+            try {
+                repository.deleteFriend(currentUserId, friendId)
+
+            } catch (e: Exception) {
+                _errorEvents.emit("Failed to delete friend: $friendId, try again!")
+            }
+        }
+    }
+
+
 }
