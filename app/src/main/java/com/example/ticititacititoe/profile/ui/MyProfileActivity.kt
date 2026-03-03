cffb8549c53aabd10a81f2a8f79f8cdf168906c1
Nedakhalaj <neda.khalaj7168@gmail.com>
2026-03-03 12:42:09 +0100

    profile is fixed now
    fixed the total game, won game and lost game

--- app/src/main/java/com/example/ticititacititoe/profile/ui/MyProfileActivity.kt
+++ app/src/main/java/com/example/ticititacititoe/profile/ui/MyProfileActivity.kt
@@ -104,21 +104,32 @@ class MyProfileActivity : AppCompatActivity() {
         val userViewModel = ViewModelProvider(this)[UserViewModel::class.java]
         userViewModel.fetchCurrentUser()
 
+        val userId = userViewModel.getCurrentUserId()
+        if (userId != null) {
+            userViewModel.fetchUserStats(userId)
+        }
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 userViewModel.currentUser.collect { user ->
                     binding.usernameTextView.text = "${user?.username} ${user?.rating}"
                     binding.initialsTextView.text = user?.username?.take(2)
-                    binding.wonGamesNumberTextView.text = user?.wonGames.toString()
-                    binding.lostGamesNumberTextView.text = user?.lostGames.toString()
-                    binding.totalGamesNumberTextView.text = user?.totalGames.toString()
                     binding.currentStreakTextView.text = getString(R.string.current_streak, user?.currentStreak)
                     binding.maxStreakTextView.text = getString(R.string.max_streak, user?.maxStreak)
 
                 }
             }
         }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                userViewModel.userStats.collect { (won, lost, total) ->
+                    binding.wonGamesNumberTextView.text = won.toString()
+                    binding.lostGamesNumberTextView.text = lost.toString()
+                    binding.totalGamesNumberTextView.text = total.toString()
+                }
+            }
+        }
+
 
     }
 }
